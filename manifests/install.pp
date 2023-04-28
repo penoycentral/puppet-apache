f1518898be80122b4b20bef6101b39ad5f269ce9
root <root@puppetserver.lab.local>
2023-04-28 16:03:32 +0000

    first manifest

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,6 +1,9 @@
-# @summary
-# Installs Apache base packages
+# @summary A short summary of the purpose of this class
 #
+# A description of what this class does
+#
+# @example
+#   include apache::install
 class apache::install {
   package { 'httpd':
     ensure => present,
