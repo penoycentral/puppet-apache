fc128c54e67aca992f279588dc38933e16c17ec6
root <root@puppetserver.lab.local>
2023-04-29 12:28:12 +0800

    service config

---
+++ manifests/service.pp
@@ -0,0 +1,15 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include apache::service
+class apache::service {
+  service { "${apache::service_name}":
+    alias      => 'apache_service',
+    ensure     => $apache::service_ensure,
+    enable     => $apache::service_enable,
+    hasrestart => true,
+    audit      => ['name', 'ensure', 'hasrestart'] 
+  }
+}
